f30cc352c7922baf6c76b016848cdaaf139e498f
Behruzbek0771 <behruzbekqalandarov771@gmail.com>
2025-05-23 04:08:05 +0500

    Bajarildi

---
+++ task22.py
@@ -0,0 +1,3 @@
+a=mahsulot_narxi=20_000.0
+b=foiz=0.15
+print(a+(a*b))  # QQS bilan mahsulot narxini hisoblash (15%)
---
+++ task23.py
@@ -0,0 +1,3 @@
+a=pul=500_000.0
+b=foiz=0.1
+print(a+(a*b)) #Bankdagi pulni yillik foiz bilan hisoblash
